8d0c0ee5f59780e01fa523056cd9b105f0188c2f
peng <1912129881@qq.com>
2024-04-24 23:41:02 +0800

    输出本代码生成的可执行程序的环境变量

--- day01/environ.c
+++ day01/environ.c
@@ -1,14 +1,16 @@
 //环境变量
-#include <stdio.h>
+#include<stdio.h>
 
-int main(void)
-{
-	extern char** environ;
-	char** pp;
-	for(pp = environ; *pp != NULL; pp++)
-	{
-		printf("%s\n", *pp);
-	}
-
-	return 0;
+int main(int argc,char* argv[],char* envp[]){
+    extern char** environ;
+    /*for(char**pp = environ;*pp;pp++){
+        printf("%s\n",*pp);
+    }*/
+    printf("environ = %p\n",environ);
+    printf("envp    = %p\n",envp);
+    for(char** pp = envp;*pp;pp++){
+        printf("%s\n",*pp);
+    }
+    return 0;
 }
+
