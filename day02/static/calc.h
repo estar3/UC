0d146e353e9e723f165204a2d35da49af673ab8f
peng <1912129881@qq.com>
2024-04-25 19:36:47 +0800

    静态库的制作、接口文件

---
+++ day02/static/calc.h
@@ -0,0 +1,9 @@
+//计算模块头文件
+#ifndef __CALC_H_
+#define __CALC_H_
+//加法计算
+int add(int a,int b);
+//减法函数
+int sub(int a,int b);
+#endif //__CALC_H_
+
---
+++ day02/static/show.c
@@ -0,0 +1,8 @@
+//显示模块实现
+#include"show.h"
+
+//显示函数
+void show(int l,char op,int r,int res){
+    printf("%d %c %d = %d\n",l,op,r,res);
+}
+
