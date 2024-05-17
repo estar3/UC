cedddffe72ea41a1e173758823e0713fe25b8149
peng <1912129881@qq.com>
2024-05-17 12:24:20 +0800

    新进程是否会继承旧进程的信号处理方式

---
+++ day13/exec.c
@@ -0,0 +1,26 @@
+//新进程是否会继承旧进程的信号处理方式
+#include<stdio.h>
+#include<unistd.h>
+#include<signal.h>
+//信号处理函数
+void sigfun(int signum){
+    printf("%d进程:捕获到%d号信号\n",getpid(),signum);
+}
+int main(void){
+    //忽略2号信号
+    if(signal(SIGINT,SIG_IGN) == SIG_ERR){
+        perror("signal");
+        return -1;
+    }
+    //捕获3号信号
+    if(signal(SIGQUIT,sigfun) == SIG_ERR){
+        perror("signal");
+        return -1;
+    }
+    //变身成new
+    if(execl("./new","new",NULL) == -1){
+        perror("execl");
+        return -1;
+    }
+    return 0;
+}
